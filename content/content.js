60829b55f69ac89aef9f40f46c8886b177da3b12
Hiroto Ishizuka <2455111t@stu.kobe-u.ac.jp>
2026-03-20 21:32:10 +0900

    feat: 常にscript、noscript、iframe要素を除去する処理を追加

--- content/content.js
+++ content/content.js
@@ -173,6 +173,7 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
         const fragment = range.cloneContents();
         const div = document.createElement('div');
         div.appendChild(fragment);
+        div.querySelectorAll('script, noscript, iframe').forEach((el) => el.remove());
         contentHtml = div.innerHTML;
       }
     }
@@ -180,6 +181,8 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
     // 通常抽出（selectionOnly 未適用の場合）
     if (contentHtml === null) {
       const documentClone = document.cloneNode(true);
+      // script・noscript・iframe は設定に関係なく常に除去
+      documentClone.querySelectorAll('script, noscript, iframe').forEach((el) => el.remove());
       removeUnwantedElements(documentClone, settings);
 
       if (settings.extractionMode === 'body') {
